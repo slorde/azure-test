319f75d3d372ef7a21095dbc679a3bc0981412c2
slorde <slorde2@gmail.com>
2025-06-15 14:15:06 -0300

    Fix

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(3000)
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
